2a040a7a34b254ba58d8717fe953516df489d52a
meysam <meysam547@gmail.com>
2016-10-26 11:10:15 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '225176317:AAEPIbJozGCNSif5ubFqKAkKjexcVQJLkIU');
+define('BOT_TOKEN', '267207612:AAE2kp8-01yBuky2wnK93Aiag82xVRyG-iQ');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
